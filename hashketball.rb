f568d1a56bf4bd256ca7e427ae651842ce50da7d
Thomas Garayua <tgarayua@gmail.com>
2020-09-30 23:24:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,32 +4,30 @@ def game_hash(home, away)
  top_level = {:home => {
    :team_name => "",
    :colors => "",
-   :players => [{
-     :player_name => { 
-     :number 
-     :shoe
-     :points
-     :rebounds
-     :assists
-     :steals
-     :blocks
-     :slam_dunks
-   ]
+   :players => {
+     :player_name => "", 
+     :number => "", 
+     :shoe => "", 
+     :points => "", 
+     :rebounds => "", 
+     :assists => "", 
+     :steals => "", 
+     :blocks =>  "", 
+     :slam_dunks =>
  }
  , :away => {
    :team_name => "",
    :colors => [""],
    :players => {
-     :player_name 
-     :number
-     :shoe
-     :points
-     :rebounds
-     :assists
-     :steals
-     :blocks
-     :slam_dunks
-   ]
+     :player_name => "", 
+     :number => "", 
+     :shoe => "", 
+     :points => "", 
+     :rebounds => "", 
+     :assists => "", 
+     :steals => "", 
+     :blocks => "", 
+     :slam_dunks => ""
  }
 end
 
