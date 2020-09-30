6af70bd2c1050e8dbb5bcbf29d1156f4a8213bd6
Thomas Garayua <tgarayua@gmail.com>
2020-09-30 23:40:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,16 +5,18 @@ def game_hash
    :home => { 
      :team_name => "", 
      :colors => "", 
-     :players => {
-     :player_name => "", 
-     :number => "", 
-     :shoe => "", 
-     :points => "", 
-     :rebounds => "", 
-     :assists => "", 
-     :steals => "", 
-     :blocks =>  "", 
-     :slam_dunks => "",
+     :players => { 
+       :player_name => "", 
+       :number => "", 
+       :shoe => "",
+       :points => "",
+       :rebounds => "",
+       :assists => "",
+       :steals => "",
+       :blocks =>  "",
+       :slam_dunks => ""
+     }
+    }
  :away => { 
    :team_name => "",
    :colors => [""],
