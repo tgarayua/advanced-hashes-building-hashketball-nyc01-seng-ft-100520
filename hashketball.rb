b83e5d3a5324fe2ce3ecc898f89909d39168abe7
Thomas Garayua <tgarayua@gmail.com>
2020-09-30 23:12:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,35 @@
 require "pry"
 
 def game_hash(home, away)
- binding.pry
+ top_level = {:home => {
+   :team_name => "",
+   :colors => "",
+   :players = [{
+     :player_name, 
+     :number, 
+     :shoe,
+     :points,
+     :rebounds,
+     :assists,
+     :steals,
+     :blocks,
+     :slam_dunks,
+   }]
+ }
+ , :away => {
+   :team_name => "",
+   :colors => [""],
+   :players => [{
+     :player_name, 
+     :number, 
+     :shoe,
+     :points,
+     :rebounds,
+     :assists,
+     :steals,
+     :blocks,
+     :slam_dunks,
+   }]
+ }
 end
 
