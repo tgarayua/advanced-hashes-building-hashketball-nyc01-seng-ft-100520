4ef8b9b4111b8c3eda0e05709b74da3268d87288
Thomas Garayua <tgarayua@gmail.com>
2020-10-01 00:00:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
        :steals => "3",
        :blocks =>  "1",
        :slam_dunks => "1" },
-      {:player_name => "Alan Anderson", 
+      {:player_name => "Reggie Evan", 
        :number => "0", 
        :shoe => "16",
        :points => "22",
