420f1caaa86635b5825df93111d4ea9545f4aa59
Thomas Garayua <tgarayua@gmail.com>
2020-10-01 00:15:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,7 +18,7 @@ def game_hash
       {:player_name => "Reggie Evans", 
        :number => 30, 
        :shoe => 14,
-       :points => 22,
+       :points => 12,
        :rebounds => 12,
        :assists => 12,
        :steals => 12,
